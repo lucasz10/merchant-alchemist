1aa85ca807def11f2bdba2c65345cac76081648d
kieferjackson <kieferleejackson@gmail.com>
2022-11-23 13:47:05 -0700

    Added package-lock to gitignore + added assets folder and style for items

--- client/src/components/item-icons/item.js
+++ client/src/components/item-icons/item.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import temp from '../../assets/sprites/temp64x64.png';
 
 function Item()
 {
+    const styles =
+    {
+        item_el_container: 
+        {
+            width: 'fit-content',
+            border: '8px solid burlywood',
+            borderStyle: 'groove',
+            backgroundColor: 'blanchedalmond'
+        }
+    }
+
     return (
         // TODO: Make sure the div has a key value!
-        <div>
+        <div className='item_el_container' style={styles.item_el_container}>
             <img 
-                className='item'
-                src={""}
+                className='item_img'
+                src={temp}
                 alt={""}
             >
                 {/* TODO: Include information such as quantity of item if viewing in potion making screen */}
