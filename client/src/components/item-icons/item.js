ad1e3e77a8a25fb6e903e89669d3a79c1b4f9192
kieferjackson <kieferleejackson@gmail.com>
2022-11-22 21:29:39 -0700

    Added components folder + item component

---
+++ client/src/components/item-icons/item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Item()
+{
+    return (
+        // TODO: Make sure the div has a key value!
+        <div>
+            <img 
+                className='item'
+                src={""}
+                alt={""}
+            >
+                {/* TODO: Include information such as quantity of item if viewing in potion making screen */}
+            </img>
+        </div>
+    )
+}
+
+export default Item;
