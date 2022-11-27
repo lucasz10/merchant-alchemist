19ea9fb9e70d11ac3648ccacabca01f4492ce2b2
kieferjackson <kieferleejackson@gmail.com>
2022-11-27 01:12:15 -0700

    Added various images and defined their sprite data

--- client/src/assets/sprites/index.js
+++ client/src/assets/sprites/index.js
@@ -1,6 +1,9 @@
 import temp from './temp64x64.png';
 import ironwoodAcorn from './ironwood-acorn64x64.png';
 import threeEyedFrog from './three-eyed-frog64x64.png';
+import gyrfalconFeathers from './gyrfalcon-feathers64x64.png';
+import crimsonHerb from './crimson-herb64x64.png';
+import opalStone from './opal-stone64x64.png';
 
 class Sprite
 {
@@ -17,6 +20,9 @@ const Sprites = {};
 // Define sprite data for each image imported
 Sprites['temp'] = new Sprite();
 Sprites['Three-eyed Frog'] = new Sprite(threeEyedFrog, 'Strange looking frog with a third eye in the center of its forehead with a celestial aura surrounding it.');
-Sprites['Ironwood Acorn'] = new Sprite(ironwoodAcorn, 'Dark gray acorn with a light gray cap and stem.')
+Sprites['Ironwood Acorn'] = new Sprite(ironwoodAcorn, 'Dark blue acorn with a light brown cap and stem.');
+Sprites['Gyrfalcon Feathers'] = new Sprite(gyrfalconFeathers, 'Three gyrfalcon feathers with light coloration and dark tips stacked on top of each other.');
+Sprites['Crimson Herb'] = new Sprite(crimsonHerb, 'An herb with crimson plant parts and furling petals which turn purple at the tips.');
+Sprites['Opal Stone'] = new Sprite(opalStone, 'A dark purple stone containing multicolored flakes which resemble a galaxy, surrounded by a white aura.');
 
 export default Sprites;
